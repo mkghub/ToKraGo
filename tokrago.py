67dca83e36cd23fd25d6df42f7659d3ab64a4632
ToKra <tomas.kratochvil@eledus.cz>
2017-08-08 18:15:04 +0200

    Clean primitive contour method

--- tokrago.py
+++ tokrago.py
@@ -3,42 +3,25 @@ import argparse
 import cv2
 import copy
 
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="Path to the input image.")
+ap.add_argument('--image', required=True,
+	help='Path to the input image.')
 args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
+image = cv2.imread(args['image'])
+image = cv2.bilateralFilter(image, 11, 17, 17)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.bilateralFilter(gray, 11, 17, 17)
 edged = cv2.Canny(gray, 30, 200)
+cv2.imshow("edged", edged)
 
-(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
-screenCnt = None
+_, cnts, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-for c in cnts:
-	# approximate the contour
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
- 
-	# if our approximated contour has four points, then
-	# we can assume that we have found our screen
-	if len(approx) == 4:
-		screenCnt = approx
-		break
-	image2 = copy.copy(image)
-	image3 = image
-	cv2.drawContours(image2, [approx], -1, (0, 255, 0), 3)
-	cv2.imshow("test", image2)	
-	cv2.waitKey(0)
-	cv2.destroyWindow("test")
+areas = [cv2.contourArea(c) for c in cnts]
+max_index = np.argmax(areas)
+cnt=cnts[max_index]
 
-
-'''
-cv2.drawContours(image, [cnts[0]], -1, (0, 255, 0), 3)
+cv2.drawContours(image, [cnt], -1, (0, 255, 0), 3)
 cv2.imshow("test", image)
 cv2.waitKey(0)
-'''
+cv2.destroyWindow("test")

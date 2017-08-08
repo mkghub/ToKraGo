0c714a4403fd08cd356d9b7e26441787e1e4a6c8
ToKra <tomas.kratochvil@eledus.cz>
2017-08-08 20:55:19 +0200

    Divide code to main 'tokrago' script and function subfiles

---
+++ edge.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+def EdgeGoban(image):
+    """Return contour of detected goban"""
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edged = cv2.Canny(gray, 30, 200)
+    cv2.imshow("edged", edged)
+
+    _, cnts, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    areas = [cv2.contourArea(c) for c in cnts]
+    max_index = np.argmax(areas)
+    cnt=cnts[max_index]
+
+    return(cnt)
--- tokrago.py
+++ tokrago.py
@@ -2,26 +2,18 @@ import numpy as np
 import argparse
 import cv2
 import copy
+from edge import EdgeGoban
 
-ap = argparse.ArgumentParser()
-ap.add_argument('--image', required=True,
-	help='Path to the input image.')
-args = vars(ap.parse_args())
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--image', required=True,
+        help='Path to the input image.')
+    args = vars(ap.parse_args())
 
-image = cv2.imread(args['image'])
-image = cv2.bilateralFilter(image, 11, 17, 17)
+    image = cv2.imread(args['image'])
+    image = cv2.bilateralFilter(image, 11, 17, 17)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-edged = cv2.Canny(gray, 30, 200)
-cv2.imshow("edged", edged)
-
-_, cnts, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-areas = [cv2.contourArea(c) for c in cnts]
-max_index = np.argmax(areas)
-cnt=cnts[max_index]
-
-cv2.drawContours(image, [cnt], -1, (0, 255, 0), 3)
-cv2.imshow("test", image)
-cv2.waitKey(0)
-cv2.destroyWindow("test")
+    cv2.drawContours(image, [EdgeGoban(image)], -1, (0, 255, 0), 3)
+    cv2.imshow("test", image)
+    cv2.waitKey(0)
+    cv2.destroyWindow("test")

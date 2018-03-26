dd089ab3ea5048d5030f3d12faa4cb06aa460b34
Tomas Kratochvil <tomas.kratochvil@eledus.cz>
2018-03-26 20:57:30 +0200

    Reorganize files

--- dependencies.py
+++
@@ -1,3 +0,0 @@
-import argparse
-import cv2
-import numpy as np
--- methods.py
+++ methods.py
@@ -1,17 +1,21 @@
-def CountourGoban(edged, image):
+import cv2
+import numpy as np
+
+
+def contour_goban(edged, image):
     _, cnts, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     areas = [cv2.contourArea(c) for c in cnts]
     max_index = np.argmax(areas)
     cnt = cnts[max_index]
 
-    imgCnt = cv2.drawContours(image.copy(), [cnt], -1, (0, 255, 0), 3)
-    return(imgCnt)
+    image_cnt = cv2.drawContours(image.copy(), [cnt], -1, (0, 255, 0), 3)
+    return image_cnt
 
 
-def HoughGoban(edged,image):
-    lines = cv2.HoughLines(edged,1,np.pi/180,180)
+def hough_goban(edged, image):
+    lines = cv2.HoughLines(edged, 1, np.pi/180, 180)
     for line in lines:
-        rho,theta = line[0]
+        rho, theta = line[0]
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a*rho
@@ -21,12 +25,12 @@ def HoughGoban(edged,image):
         x2 = int(x0 - 1000*(-b))
         y2 = int(y0 - 1000*(a))
 
-        cv2.line(image,(x1,y1),(x2,y2),(0,0,255),2)
+        cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-    return(image)
+    return image
 
 
-def ShowImage(images, imageWindowNames):
+def show_image(images, imageWindowNames):
     for image, imageWindowName in zip(images, imageWindowNames):
         cv2.imshow(imageWindowName, image)
     cv2.waitKey(0)
